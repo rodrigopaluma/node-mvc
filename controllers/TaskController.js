7bf49c59fcc561e80549333be5566725df49d25e
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-10 13:01:23 -0300

    Finalizando as tarefas dentro do sistema.

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -51,6 +51,20 @@ module.exports = class TaskController {
         res.redirect('/tasks');        
     }
 
+    static async updateStatus(req, res) {
+        const id = req.body.id;
+        const task = {
+            done: req.body.done === '0' ? true : false
+        }
+
+        await Task.update(task, {
+            where: {
+                id: id
+            }
+        });
+        res.redirect('/tasks');
+    }
+
     static async showTasks(req, res) {
         const tasks = await Task.findAll({raw:true});
         res.render('tasks/allTasks',{tasks});
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -8,6 +8,7 @@ router.post('/add', TaskController.createTaskSave);
 router.post('/remove', TaskController.removeTask);
 router.get('/edit/:id', TaskController.updateTask);
 router.post('/edit', TaskController.updateTaskSave);
+router.post('/updatestatus', TaskController.updateStatus);
 router.get('/', TaskController.showTasks);
 
 module.exports = router;
